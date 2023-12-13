5265bb3fafd9bae75eef0f4535707064de2636a8
Theodore Keloglou <zf@sirodoht.com>
2023-12-13 21:48:23 +0000

    add user model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    email = models.EmailField(_("email address"), blank=True, unique=True)
+    about = models.CharField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
--- zeroshot/settings.py
+++ zeroshot/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "zeroshot.wsgi.application"
 
+AUTH_USER_MODEL = "main.User"
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
